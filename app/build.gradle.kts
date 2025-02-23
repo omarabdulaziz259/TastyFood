b856f1f699dec3970cb58209fab580ce30041a66
omar abdelaziz <omarabdelaziz259@gmail.com>
2025-02-23 18:36:32 +0200

    google sign in feature added and handled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,7 @@ dependencies {
     implementation ("com.airbnb.android:lottie:6.6.2")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.google.android.gms:play-services-auth:21.3.0")
-    implementation ("androidx.credentials:credentials:1.3.0")
-    implementation ("androidx.credentials:credentials-play-services-auth:1.3.0")
-    implementation ("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation ("com.google.firebase:firebase-auth:22.3.1")
 
 
 }
--- app/src/main/java/com/example/tastyfood/welcome/view/WelcomeFragment.java
+++ app/src/main/java/com/example/tastyfood/welcome/view/WelcomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.tastyfood.welcome.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,16 +16,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tastyfood.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class WelcomeFragment extends Fragment {
 
     FirebaseAuth  mAuth;
     GoogleSignInClient googleSignInClient;
+    private ActivityResultLauncher<Intent> googleSignInLauncher;
+
+    private static final int RC_SIGN_IN = 100;
+    NavController navController;
 
     Button btnSignInWithEmail, btnSignInWithGoogle, btnAsAGuest;
     TextView txtSignUp;
@@ -50,6 +66,29 @@ public class WelcomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
+
+        googleSignInLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == getActivity().RESULT_OK && result.getData() != null) {
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                        try {
+                            GoogleSignInAccount account = task.getResult(ApiException.class);
+                            firebaseAuthWithGoogle(account.getIdToken());
+                        } catch (ApiException e) {
+                            Toast.makeText(requireContext(), "Google Sign-In Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
         btnSignInWithEmail = view.findViewById(R.id.btnSignInWithEmail);
         btnAsAGuest = view.findViewById(R.id.btnGuest);
         btnSignInWithGoogle = view.findViewById(R.id.btnSignInWithGoogle);
@@ -57,5 +96,33 @@ public class WelcomeFragment extends Fragment {
         NavController navController = Navigation.findNavController(view);
         btnSignInWithEmail.setOnClickListener(v -> navController.navigate(R.id.action_welcomeFragment_to_signInFragment));
         txtSignUp.setOnClickListener(v -> navController.navigate(R.id.action_welcomeFragment_to_signUpFragment));
+
+        btnSignInWithGoogle.setOnClickListener(v -> signIn());
     }
+
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        googleSignInLauncher.launch(signInIntent);
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        updateUI(null);
+                    }
+                });
+    }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            navController.navigate(R.id.action_welcomeFragment_to_userProfileFragment);
+        } else {
+            Toast.makeText(requireContext(), "Sign-In Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
